54ad5a5d7283d9c336617443a0e88a2eeeb11f6d
kabra7170-stack <kabra7170@gmail.com>
2026-05-23 02:33:25 -0400

    fix: gooooooooooooooooooooooooo

--- api/nexsys.php
+++ api/nexsys.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(200); exit; }
 require_once '../config/database.php';
 require_once '../config/session.php';
 
